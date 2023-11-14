23995ff9b8c235bbc1a6fa535eae05d496d0184a
gwaxg <andrei.mitriakov@gmail.com>
2023-11-15 00:29:54 +0100

    Charts!

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { NgChartsModule } from 'ng2-charts';
-
+import { NgApexchartsModule } from 'ng-apexcharts';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PredictionsComponent } from './components/predictions/predictions.component';
 import { KeyvaluePipe } from './pipes/keyvalue.pipe';
 import { HistoryComponent } from './components/history/history.component';
+import { ChartsComponent } from './components/charts/charts.component';
 
 @NgModule({
   declarations: [
@@ -16,12 +17,14 @@ import { HistoryComponent } from './components/history/history.component';
     KeyvaluePipe,
     HistoryComponent,
     PredictionsComponent,
+    ChartsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    NgApexchartsModule,
     NgChartsModule.forRoot()
   ],
   providers: [],
